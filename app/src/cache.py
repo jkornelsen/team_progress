8ef89aed2e705d98133787f292f7096da885e2b7
Jim Kornelsen <jk.kornelsen@gmail.com>
2024-10-02 11:22:52 -0500

    First testing code.

--- app/src/cache.py
+++ app/src/cache.py
@@ -1,6 +1,6 @@
 from flask_caching import Cache
 
-cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache = Cache(config={'CACHE_TYPE': 'flask_caching.backends.SimpleCache'})
 
 def init_cache(app):
     cache.init_app(app)
